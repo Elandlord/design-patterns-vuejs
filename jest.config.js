ecd643df176803e9cffd47877cebac7d7fb789ce
Eric Landheer <e.landheer@mentechmedia.nl>
2021-07-19 15:46:44 +0200

    :white_check_mark: Add HTTP and API + service worker mocking

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    testEnvironment: 'jsdom',
-    transform: {
-        "^.+\\.vue$": "vue-jest",
-        "^.+\\js$": "babel-jest"
-    },
-    moduleFileExtensions: ['vue', 'js', 'json', 'jsx', 'ts', 'tsx', 'node']
+  testEnvironment: 'jsdom',
+  transform: {
+    "^.+\\.vue$": "vue-jest",
+    "^.+\\js$": "babel-jest"
+  },
+  moduleFileExtensions: ['vue', 'js', 'json', 'jsx', 'ts', 'tsx', 'node']
 }
